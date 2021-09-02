b6b14fcd3236a8e0a6762e4de2ec6365163e3233
Benedict Harcourt <ben.harcourt@myvouchercodes.co.uk>
2021-09-02 14:41:36 +0000

    Improve logging

--- write-env-file.js
+++ write-env-file.js
@@ -11,10 +11,12 @@ const PREFIX = "INPUT_ENVKEY_";
 async function main() {
     const file_name = core.getInput("file_name");
     const workspace = process.env.GITHUB_WORKSPACE;
+    const env_path = path.join(workspace, file_name);
 
-    const stream = fs.createWriteStream(path.join(workspace, file_name));
+    core.info("Writing to " + env_path);
+    const stream = fs.createWriteStream(env_path);
 
-    stream.on("finish", () => core.debug(file_name + " written"));
+    stream.on("finish", () => core.info(file_name + " written"));
 
     Object.keys(process.env)
         .filter( k => process.env.hasOwnProperty(k) )
