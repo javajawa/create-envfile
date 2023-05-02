0f4ec2c9bd2ba5a6d4afadaf89bf42c65caa820f
jinja144 <benjamin.watson.1995@gmail.com>
2023-05-02 15:23:03 +0100

    Eslint fixes

--- write-env-file.js
+++ write-env-file.js
@@ -4,28 +4,30 @@
 
 const core = require("@actions/core");
 const path = require("path");
-const fs   = require("fs");
+const fs = require("fs");
 
 const PREFIX = "INPUT_ENVKEY_";
 
 async function main() {
-    const file_name = core.getInput("file_name");
-    const workspace = process.env.GITHUB_WORKSPACE;
-    const env_path = path.join(workspace, file_name);
+  const file_name = core.getInput("file_name");
+  const workspace = process.env.GITHUB_WORKSPACE;
+  const env_path = path.join(workspace, file_name);
 
-    core.info("Writing to " + env_path);
-    const stream = fs.createWriteStream(env_path);
+  core.info("Writing to " + env_path);
+  const stream = fs.createWriteStream(env_path);
 
-    stream.on("finish", () => core.info(file_name + " written"));
+  stream.on("finish", () => core.info(file_name + " written"));
 
-    Object.keys(process.env)
-        .filter( k => process.env.hasOwnProperty(k) )
-        .filter( k => k.startsWith(PREFIX) )
-        .forEach( k => stream.write(`${k.substr(PREFIX.length)}=${process.env[k]}\n`))
+  Object.keys(process.env)
+    .filter((k) => process.env.hasOwn(k))
+    .filter((k) => k.startsWith(PREFIX))
+    .forEach((k) =>
+      stream.write(`${k.slice(PREFIX.length)}=${process.env[k]}\n`)
+    );
 
-    stream.end();
+  stream.end();
 }
 
-main().catch(error => core.setFailed(error.message));
+main().catch((error) => core.setFailed(error.message));
 
 // vim: nospell ts=4 expandtab
